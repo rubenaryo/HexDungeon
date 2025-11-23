572e841d9cae81c374d9e6d143433546e70f0c12
Ruben Young <rubeny484@gmail.com>
2025-11-23 01:36:42 -0500

    Add Hex type adjacencies

--- src/HexData.ts
+++ src/HexData.ts
@@ -1,14 +1,44 @@
 // Main data structure for hexes behavior
 
+enum TileType
+{
+    INVALID = -1,
+    WATER = 0,
+    SAND = 1,
+    GRASS = 2
+};
+
+function getPossibleNeighborTypes(t: TileType): TileType[]
+{
+    let ret = [];
+    switch(t)
+    {
+        case TileType.INVALID:
+            ret.push(TileType.WATER, TileType.SAND, TileType.GRASS);
+            break;
+        case TileType.WATER:
+            ret.push(TileType.WATER, TileType.SAND);
+            break;
+        case TileType.SAND:
+            ret.push(TileType.WATER, TileType.SAND, TileType.GRASS);
+            break;
+        case TileType.GRASS:
+            ret.push(TileType.SAND, TileType.GRASS);
+            break;
+    }
+
+    return ret;
+}
+
 class HexData {
     q: number;
     r: number;
-    tileType: string | null; // TODO: enum
+    type: TileType;
 
     constructor(q: number, r: number) {
         this.q = q;
         this.r = r;
-        this.tileType = null;
+        this.type = TileType.INVALID;
     }
 }
 
